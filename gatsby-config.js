b47b05a7499ac6f090582243171f46451a7d9f74
Harry <harrytaylordeveloper@gmail.com>
2021-03-23 21:50:54 +1300

    edit netlify cms reslove

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,9 +66,7 @@ module.exports = {
     },
     {
       resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
+      
     },
     {
       resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
